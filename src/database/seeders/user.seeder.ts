5b85073b1f47c38c16ce32cdc54e0a83758aa46e
noobj <jingfu99@gmail.com>
2021-08-13 09:03:05 +0800

    test: add login test #3

---
+++ src/database/seeders/user.seeder.ts
@@ -0,0 +1,18 @@
+import userModel from 'src/modules/users/user.model';
+
+class UserSeeder {
+    static async run() {
+        await Promise.all([userModel.createCollection(), userModel.deleteMany()]);
+        await userModel.insertMany(users);
+    }
+}
+
+const users = [
+    {
+        _id: 1,
+        account: 'jjj',
+        password: '$2b$10$N45EGR5JNu8LlA.VPn5ioe4RxO2XYk0L0PW.vVSxYtS84sBU.Nvye'
+    }
+];
+
+export default UserSeeder;
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'src/modules/entries/entry.controller';
+import 'src/modules/auth/auth.controller';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
@@ -8,16 +9,19 @@ import { InversifyExpressServer } from 'inversify-express-utils';
 import { Container } from 'inversify';
 import EntryService from 'src/modules/entries/entry.service';
 import express from 'express';
+import session from 'express-session';
 import request from 'supertest';
 import dotenv from 'dotenv';
 import { join } from 'path';
 import EntrySeeder from 'src/database/seeders/entry.seeder';
 import CategorySeeder from 'src/database/seeders/category.seeder';
+import UserSeeder from 'src/database/seeders/user.seeder';
 
 dotenv.config({ path: join(__dirname, '../.env.example') });
 
 describe('EntryController (e2e)', () => {
     let app: express.Application;
+    let cookies;
 
     beforeAll(async (done) => {
         // DB initialize and seeding
@@ -31,7 +35,7 @@ describe('EntryController (e2e)', () => {
             console.log(e);
         }
 
-        await Promise.all([EntrySeeder.run(), CategorySeeder.run()]);
+        await Promise.all([EntrySeeder.run(), CategorySeeder.run(), UserSeeder.run()]);
 
         // Server initialize
         const container = new Container();
@@ -39,17 +43,42 @@ describe('EntryController (e2e)', () => {
         const server = new InversifyExpressServer(container);
         server.setConfig((app) => {
             app.use(bodyParser.json());
-            app.use(cookieParser());
             app.use(errorMiddleware);
+            app.use(
+                session({
+                    secret: process.env.COOKIE_SECRET,
+                    resave: false,
+                    saveUninitialized: false,
+                    cookie: { maxAge: 600 * 1000 }
+                })
+            );
         });
 
         app = server.build();
         done();
     });
 
+    it('/POST auth/login', (done) => {
+        const payload = {
+            account: 'jjj',
+            password: '1234'
+        };
+
+        return request(app)
+            .post('/auth/login')
+            .send(payload)
+            .end(function (err, res) {
+                expect(res.status).toEqual(200);
+                // Save the cookie to use it later to retrieve the session
+                cookies = res.headers['set-cookie'].pop().split(';')[0];
+                done();
+            });
+    });
+
     it('/Get entries', (done) => {
         return request(app)
             .get('/entries')
+            .set('Cookie', cookies)
             .query({
                 timeStart: '2021-07-01',
                 timeEnd: '2021-07-31',
